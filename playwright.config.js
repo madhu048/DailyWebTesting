a30b7d2542a16dea961f04b8ca78d6be49ba0058
ESK0060 <ESK0060@IMX-LAP-005.kastechssg.local>
2025-07-23 19:57:50 +0530

    Few web links have been added for load testing

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
   },
-  timeout:200000,
+  timeout:700000,
 
   /* Configure projects for major browsers */
   projects: [
--- tests/webtesting.spec.js
+++ tests/webtesting.spec.js
@@ -4,30 +4,90 @@ async function urlStatus(page,url){
     try {
         const response = await page.goto(url);
         try {
-                    await page.waitForLoadState('load', { timeout: 90000 }); // try for 90s
+                    await page.waitForLoadState('load', { timeout: 20000 }); // try for 20s
             } catch (e) {
-                    console.warn(`⚠️ ${url} got Skipped because is taking long time to load.`);
+                    console.warn(`⚠️ ${url} took too long to load fully. Continuing...`);
             }
         await page.waitForTimeout(2000);
         let statusCode = await response.status();
-        console.log(`${url} status code is: ${await statusCode}`);
         if(statusCode < 400){
             expect.soft(statusCode).toBeLessThan(400);
-            console.log(`✅ ${url} is loading fine.`);
+            console.log(`✅ ${await statusCode}-${url} is loading fine.`);
             return true;
         }else{
             expect.soft(false).toBeTruthy();
-            console.log(`⚠️ There is a isssue with url: ${url}`)
+            console.log(`⚠️ ${statusCode}-There is a isssue with url: ${url}`)
             return false;
         }
     } catch (error) {
-        console.error(`❌ There is issue with Url : ${error}`);
+        console.error(`❌ There is issue with Url: ${url}: ${error}`);
         expect.soft(false).toBeTruthy();
         return false;
     }
 }
 
+const links = {imaginX:"https://www.imaginxavr.com/",
+               CrystalStructure:"https://uh.imaginxavr.com/crystal-structure/",
+               WaveDiffraction:"https://uh.imaginxavr.com/wave-diffraction/",
+               CrystalBindingAndElastic:"https://uh.imaginxavr.com/crystal-binding/",
+               CrystalVibrations:"https://uh.imaginxavr.com/crystal-vibrations/",
+               ThermalProperties:"https://uh.imaginxavr.com/thermalproperties/",
+               FreeElectronFermiGas:"https://uh.imaginxavr.com/fermigas/",
+               EnergyBands:"https://uh.imaginxavr.com/energyband/",
+               SemiconductorCrystals:"https://uh.imaginxavr.com/semi-conductor-crystals/",
+               FermiSurfacesAndMetals:"https://uh.imaginxavr.com/fermi-surfaces-and-metals/",
+               LabSafety:"https://uh.imaginxavr.com/labsafety/",
+               EnzymeKinetics:"https://uh.imaginxavr.com/enzyme-kinetics/",
+               GeneObesity:"https://uh.imaginxavr.com/geneobesity/",
+               GeneRegulation:"https://uh.imaginxavr.com/generegulation/",
+               mRNA:"https://uh.imaginxavr.com/mRNA/",
+               NextGeneSequencing:"https://uh.imaginxavr.com/next-generation-sequencing/",
+               PCR:"https://uh.imaginxavr.com/next-generation-sequencing/",
+               UniversityOfHouston:"https://uh.imaginxavr.com/uh-web/",
+               CellDevision:"https://experience.imaginxavr.com/cell-division/",
+               DNA:"https://experience.imaginxavr.com/dna-stem/",
+               Electricity:"https://experience.imaginxavr.com/electricity-stem/",
+               Lipids:"https://experience.imaginxavr.com/lipids-stem/",
+               LipidsDemolink:"https://experience.imaginxavr.com/lipids-stem/",
+               NursingIVinsertion:"https://experience.imaginxavr.com/nursing-IV-insertion/",
+               ClientVideos:"https://experience.imaginxavr.com/ClientVideos/",
+               ixWeb:"https://experience.imaginxavr.com/ix-web/",
+               Best:"http://experience.imaginxavr.com/best/",
+               Bestv2:"http://experience.imaginxavr.com/best_v2/",
+               Heart:"https://experience.imaginxavr.com/explore_human_heart/",
+               HeartLimited:"https://experience.imaginxavr.com/human-heart-limited-version/",
+               Pimav1Unityweb:"http://experience.imaginxavr.com/pima_v1/",
+               AnatomyCourseWeb:"https://experience.imaginxavr.com/anatomy_course/",
+               HumanAnatomyUnityWeb:"http://experience.imaginxavr.com/human_anatomy/",
+               AviationCourseWeb:"http://experience.imaginxavr.com/aviation_course/",
+               CessnaWheelUnityWeb:"http://experience.imaginxavr.com/cessna_wheel/",
+               EngineeringCourseWeb:"http://experience.imaginxavr.com/engineering_course/",
+               JetEngineUnityWeb:"http://experience.imaginxavr.com/jet_engine/",
+               HorizonWeb:"http://experience.imaginxavr.com/horizon/",
+               HorizonUnityWeb:"https://experience.imaginxavr.com/horizon_webgl/",
+               HorizonChemistryWeb:"http://experience.imaginxavr.com/horizon_chemistry/",
+               ixTestUnityWeb:"http://experience.imaginxavr.com/ix_test/",
+               NathanHaleAppWeb:"http://nhs.imaginxavr.com/web/",
+               UtahAppWeb:"https://utahforgevirtualtour.imaginxavr.com/",
+               UtahAnalyticsAppWeb:"https://utahforgevirtualtour.imaginxavr.com/utah_backend/dashboard",
+               UtahClientBackEndLinkWeb:"https://utahforgevirtualtour.imaginxavr.com/utah_backend/",
+               UtahClientBackEndLinkWebSharedWithClient:"https://experience.imaginxavr.com/utah_backend/",
+               TDWAppWeb:"http://experience.imaginxavr.com/tdw_webgl/",
+               TDWAnalyticsWeb:"https://experience.imaginxavr.com/tdw-analytics/admin",
+               
+               
+            }
+
+
 test('Url checking',async({page})=>{
-        const url = "https://www.imaginxavr.com/";
-        await urlStatus(page,url);
+
+    const keys = Object.keys(links);
+    console.log(`Total links: ${keys.length}`);
+    for(let i=0;i < keys.length;i++){
+        const key = keys[i];
+        const value = links[keys[i]];
+        console.log(`${key}: ${value}.`);
+        await urlStatus(page,value);
+    }
+        
     });
