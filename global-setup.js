1c79fbcdf1b6533c73bd33eb75427e6211135ea3
ESK0060 <ESK0060@IMX-LAP-005.kastechssg.local>
2025-07-24 18:13:11 +0530

    Code updated to crear the report folder

--- global-setup.js
+++ global-setup.js
@@ -2,11 +2,18 @@ const fs = require('fs-extra');
 
 module.exports = async () => {
   const folderPath = 'screenshots';
+  const playwrightReport = 'playwright-report';
   try {
     await fs.emptyDir(folderPath); // Clears all files from screenshots folder
     console.log(`🧹 Cleared '${folderPath}' folder.`);
   } catch (err) {
     console.error(`❌ Error clearing '${folderPath}':`, err);
+  }
+  try {
+    await fs.emptyDir(playwrightReport); // Clears all files from playwrightReport folder
+    console.log(`🧹 Cleared '${playwrightReport}' folder.`);
+  } catch (err) {
+    console.error(`❌ Error clearing '${playwrightReport}':`, err);
   }
 
 } 
